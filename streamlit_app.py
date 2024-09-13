788c4ae9eab637f1c5a11cc81c4486a5ee4eba73
Wamei154 <57544490+Wamei154@users.noreply.github.com>
2024-09-14 00:25:19 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,13 @@
 import os
-import re
 import streamlit as st
 from openpyxl import load_workbook
-from datetime import datetime, timedelta
+from datetime import datetime
 import pandas as pd
+import time 
 import pytz
 
-
 def resource_path(relative_path):
-    """Return the absolute path to the resource."""
+    """ Return the absolute path to the resource """
     base_path = os.path.abspath(".")
     return os.path.join(base_path, relative_path)
 
@@ -19,7 +18,7 @@ def flatten_list(nested_list):
 
 @st.cache_data
 def load_data(classe):
-    """Load data from Excel files based on the selected class."""
+    """ Load data from Excel files based on the selected class """
     colloscope_file = resource_path(f'Colloscope{classe}.xlsx')
     legende_file = resource_path(f'Legende{classe}.xlsx')
 
@@ -28,10 +27,6 @@ def load_data(classe):
 
     sheet_colloscope = excel_colloscope.active
     sheet_legende = excel_legende.active
-
-    # Récupérer les valeurs entre parenthèses de la première ligne
-    dates_row = [extract_date_from_cell(cell.value) for cell in sheet_colloscope[1]]
-    print(f"Dates extraites de la première ligne : {dates_row}")
 
     data_dict = {}
     data_dict1 = {}
@@ -48,46 +43,31 @@ def load_data(classe):
         values1 = [v.split() if v is not None else [] for v in values1]
         data_dict1[key1] = values1
 
-    return data_dict, data_dict1, dates_row
+    return data_dict, data_dict1
 
 
-def extract_date_from_cell(cell_value):
-    """Extract the date from a cell value if it's in parentheses."""
-    if isinstance(cell_value, str):
-        match = re.search(r'\((\d{2}/\d{2})\)', cell_value)
-        if match:
-            return match.group(1)
-    return None
+def get_current_week():
+    now = datetime.now()
+    current_week = now.isocalendar()[1]
+    return min(current_week, 30)
 
 
-def get_current_date():
-    """Get the current date in format %d/%m and add 2 days."""
-    timezone = pytz.timezone('Europe/Paris')
-    current_date = datetime.now(timezone)
-    new_date = current_date + timedelta(days=2)
-    return new_date.strftime("%d/%m")
+def save_settings(groupe, semaine, classe):
+    with open('config.txt', 'w') as f:
+        f.write(f"{groupe}\n{semaine}\n{classe}")
 
-
-def load_last_updated_date():
-    """Load the last updated date from a file."""
-    if os.path.exists('last_updated.txt'):
-        with open('last_updated.txt', 'r') as f:
-            return f.read().strip()
-    return None
-
-
-def save_current_date(date):
-    """Save the current date to a file."""
-    with open('last_updated.txt', 'w') as f:
-        f.write(date)
-
-
-def compare_dates_with_columns(dates_row, current_date):
-    """Compare the dates from the first row with the current date."""
-    for idx, date in enumerate(dates_row[1:], start=1):  # Ignorer la colonne 0 (index)
-        if date and date == current_date:
-            return idx  # Retourner le numéro de colonne correspondant (B = 1, C = 2, etc.)
-    return None
+def load_settings():
+    groupe = "G10"
+    semaine = str(get_current_week())
+    classe = "1"  # Default class
+    if os.path.exists('config.txt'):
+        with open('config.txt', 'r') as f:
+            lines = f.readlines()
+            if len(lines) >= 3:
+                groupe = lines[0].strip()
+                semaine = lines[1].strip()
+                classe = lines[2].strip()
+    return groupe, semaine, classe
 
 
 def colo(groupe, semaine, data_dict, data_dict1):
@@ -129,30 +109,6 @@ def colo(groupe, semaine, data_dict, data_dict1):
     return m
 
 
-def save_settings(groupe, semaine, classe):
-    with open('config.txt', 'w') as f:
-        f.write(f"{groupe}\n{semaine}\n{classe}")
-
-
-def load_settings():
-    groupe = "G10"
-    semaine = str(get_current_week())
-    classe = "1"  # Default class
-    if os.path.exists('config.txt'):
-        with open('config.txt', 'r') as f:
-            lines = f.readlines()
-            if len(lines) >= 3:
-                groupe = lines[0].strip()
-                semaine = lines[1].strip()
-                classe = lines[2].strip()
-    return groupe, semaine, classe
-
-
-def get_current_week():
-    now = datetime.now()
-    current_week = now.isocalendar()[1]
-    return min(current_week, 30)
-
 
 def display_data():
     groupe = st.session_state.groupe
@@ -176,37 +132,11 @@ def display_data():
             st.error("Le Groupe doit être entre 1 et 20.")
             return
     except ValueError:
-        st.error("Veuillez entrer un Groupe valide entre 1 et 20 et de commencer par 'G' comme G10.")
+        st.error("Veuillez entrer un Groupe valide entre 1 et 20 et de commencer par "'G'" comme G10.")
         return
 
-    data_dict, data_dict1, dates_row = load_data(classe)
+    data_dict, data_dict1 = load_data(classe)
 
-    # Obtenir la date actuelle avec 3 jours ajoutés
-    current_date = get_current_date()
-    st.write(f"Date actuelle (avec 3 jours ajoutés) : {current_date}")
-
-    # Charger la dernière date mise à jour
-    last_updated_date = load_last_updated_date()
-    st.write(f"Date de la dernière mise à jour : {last_updated_date}")
-
-    # Mettre à jour si la date a changé
-    if last_updated_date != current_date:
-        save_current_date(current_date)
-        st.write(f"La date actuelle a été mise à jour : {current_date}")
-    else:
-        st.write(f"La date actuelle est : {current_date}")
-
-    # Comparer les dates de la première ligne avec la date actuelle
-    matching_column = compare_dates_with_columns(dates_row, current_date)
-    st.write(f"Colonne correspondant à la date actuelle : {matching_column}")
-
-    # Mettre à jour le numéro de semaine dans la session et dans la barre latérale si une colonne correspond
-    if matching_column is not None:
-        semaine = matching_column  # Ajuster à 0-indexé pour la semaine
-        st.session_state.semaine = str(semaine)
-        st.sidebar.text_input("Semaine", value=str(semaine))
-
-    # Utiliser la semaine mise à jour
     data = colo(groupe, semaine, data_dict, data_dict1)
 
     df = pd.DataFrame(data, columns=["Professeur", "Jour", "Heure", "Salle"])
@@ -214,28 +144,21 @@ def display_data():
 
     st.table(df.style.hide(axis='index'))
 
-
 def main():
     st.sidebar.header("Sélection")
-
-    # Charger les paramètres et vérifier qu'ils sont valides
-    settings = load_settings()
-    if not all(isinstance(x, str) for x in settings):
-        st.error("Les paramètres chargés depuis le fichier de configuration sont invalides.")
-        return
-
+    
     classe = st.sidebar.selectbox("TSI", options=["1", "2"], index=0)
-    groupe = st.sidebar.text_input("Groupe", value=settings[0])
-    semaine = st.sidebar.text_input("Semaine", value=settings[1])
-
-    if st.sidebar.button("Télécharger le fichier EXE", 'https://drive.google.com/drive/folders/1EiyTE39U-jhlz4S8Mtun3qG04IG0_Gxn?usp=sharing'):
+    groupe = st.sidebar.text_input("Groupe", value=load_settings()[0])
+    semaine = st.sidebar.text_input("Semaine", value=load_settings()[1])
+    
+    if st.sidebar.button("Télécharger le fichier EXE", 'https://drive.google.com/drive/folders/1EiyTE39U-jhlz4S8Mtun3qG04IG0_Gxn?usp=sharing' ):
         st.sidebar.markdown(
-            'En Construction',
+            f'En Construction',
             unsafe_allow_html=True
         )
 
     st.sidebar.button("Afficher", on_click=display_data)
-
+    
     st.sidebar.markdown(
         """
         <div style="position: fixed; bottom: 0; width: 100%; text-align: center; font-size: 10px;">
@@ -244,11 +167,9 @@ def main():
         """,
         unsafe_allow_html=True
     )
-
     st.session_state.groupe = groupe
     st.session_state.semaine = semaine
-    st.session_state.classe = classe
-
-
+    st.session_state.classe = classe  
+    
 if __name__ == "__main__":
     main()
