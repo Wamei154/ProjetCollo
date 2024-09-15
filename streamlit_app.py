ab6933a945c32773b04d3adb47ed26444ea75dd8
Wamei154 <57544490+Wamei154@users.noreply.github.com>
2024-09-15 18:22:07 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -161,7 +161,7 @@ def main():
     
     st.markdown(
         """
-        <div style="position: fixed; bottom: 0; width: 100%; text-align: center; font-size: 10px;">
+        <div style="position: fixed; bottom: 100; width: 100%; text-align: center; font-size: 10px;">
             Fait par BERRY Mael, avec l'aide de SOUVELAIN Gauthier et de DAMBRY Paul
         </div>
         """,
