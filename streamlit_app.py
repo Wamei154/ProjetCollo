7cc57ce4dff6e3013569d60cd0bf687a5852ca81
Wamei154 <57544490+Wamei154@users.noreply.github.com>
2024-09-15 19:02:51 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -205,8 +205,8 @@ def main():
         """,
         unsafe_allow_html=True
     )
-    contenu_message = st.sidebar.button("?"):
-        st.text_area("Entrez votre message ici")
+    if st.sidebar.button("?"):
+        contenu_message = st.text_area("Entrez votre message ici")
 
     # Bouton d'envoi
     if st.sidebar.button("Envoyer"):
