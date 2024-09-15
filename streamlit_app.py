08b4d61d4a573bd70dc6fbecbe9d05968c3da9a6
Wamei154 <57544490+Wamei154@users.noreply.github.com>
2024-09-15 19:21:33 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -201,7 +201,7 @@ def main():
         unsafe_allow_html=True
     )
 
-    if st.popover(Erreur?):
+    if st.popover('Erreur?'):
         with st.form("chat_form"):
             contenu_message = st.text_area("Entrez votre message ici")
             submit = st.form_submit_button("Envoyer")
