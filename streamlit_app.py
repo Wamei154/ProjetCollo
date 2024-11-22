0c98ae04df25658166faadb0293251334f781911
Wamei154 <57544490+Wamei154@users.noreply.github.com>
2024-11-22 19:56:52 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,22 +2,23 @@ import os
 import streamlit as st
 from openpyxl import load_workbook
 import pandas as pd
-import time
 from datetime import datetime, timedelta
 
+
 def resource_path(relative_path):
-    """ Return the absolute path to the resource """
+    """Return the absolute path to the resource."""
     base_path = os.path.abspath(".")
     return os.path.join(base_path, relative_path)
 
 
 def flatten_list(nested_list):
+    """Flatten a nested list."""
     return [' '.join(inner_list) for inner_list in nested_list]
 
 
 @st.cache_data
 def load_data(classe):
-    """ Load data from Excel files based on the selected class """
+    """Load data from Excel files based on the selected class."""
     colloscope_file = resource_path(f'Colloscope{classe}.xlsx')
     legende_file = resource_path(f'Legende{classe}.xlsx')
 
@@ -45,50 +46,29 @@ def load_data(classe):
     return data_dict, data_dict1
 
 
-def get_current_week():
-    now = datetime.now()
-    current_week = now.isocalendar()[1]
-    return min(current_week, 30)
-
-
-def save_settings(groupe, semaine, classe):
-    with open('config.txt', 'w') as f:
-        f.write(f"{groupe}\n{semaine}\n{classe}")
-
-
-def load_settings():
-    groupe = "G10"
-    semaine = str(get_current_week())
-    classe = "1"  # Default class
-    if os.path.exists('config.txt'):
-        with open('config.txt', 'r') as f:
-            lines = f.readlines()
-            if len(lines) >= 3:
-                groupe = lines[0].strip()
-                semaine = lines[1].strip()
-                classe = lines[2].strip()
-    return groupe, semaine, classe
+def get_week_start_date(start_date, current_date):
+    """
+    Calculate the start date of the current or next week.
+    If the current date is outside the interval, move to the next week.
+    """
+    while current_date > start_date + timedelta(days=6):  # If outside the interval
+        start_date += timedelta(days=7)  # Move to the next week
+    return start_date
 
 
 def colo(groupe, semaine, data_dict, data_dict1):
     m = []
-
     try:
         if groupe not in data_dict:
             raise KeyError(f"Le groupe '{groupe}' n'existe pas dans les données.")
-
         if semaine - 1 >= len(data_dict[groupe]) or semaine - 1 < 0:
             raise IndexError(f"La semaine {semaine} n'est pas valide pour le groupe '{groupe}'.")
-
         s = data_dict[groupe][semaine - 1]
-
         for k in range(len(s)):
             if s[k] not in data_dict1:
                 raise KeyError(f"La clé '{s[k]}' n'existe pas dans les données de légende.")
-
             joined_elements = flatten_list(data_dict1[s[k]])
             matiere = "Non spécifié"
-
             if s[k].startswith('M'):
                 matiere = "Mathématiques"
             elif s[k].startswith('A'):
@@ -101,42 +81,24 @@ def colo(groupe, semaine, data_dict, data_dict1):
                 matiere = "Informatique"
             elif s[k].startswith('P'):
                 matiere = "Physique"
-
             joined_elements.append(matiere)
             m.append(joined_elements)
-
     except KeyError as e:
         st.error(str(e))
         return m
-
     except IndexError as e:
         st.error(str(e))
         return m
-
     except Exception as e:
         st.error(f"Une erreur inattendue s'est produite : {str(e)}")
         return m
-
     return m
-
-
-def get_week_start_date(start_date, current_date):
-    """
-    Calcule le premier jour de la semaine courante ou suivante.
-    Si la date actuelle n'est pas dans un intervalle d'une semaine, elle passe à la semaine suivante.
-    """
-    while current_date > start_date + timedelta(days=6):  # Si hors intervalle de la semaine actuelle
-        start_date += timedelta(days=7)  # Passer à la semaine suivante
-    return start_date.strftime("%d/%m/%Y")  # Format JJ/MM/AAAA
-
 
 
 def display_data():
     groupe = st.session_state.groupe
     semaine = st.session_state.semaine
     classe = st.session_state.classe
-
-    save_settings(groupe, semaine, classe)
 
     try:
         semaine = int(semaine)
@@ -149,68 +111,51 @@ def display_data():
 
     try:
         group_number = int(groupe[1:])
-        if group_number < 0 or group_number > 20:
+        if group_number < 1 or group_number > 20:
             st.error("Le Groupe doit être entre 1 et 20.")
             return
     except ValueError:
-        st.error("Veuillez entrer un Groupe valide entre 1 et 20 et de commencer par 'G' comme G10.")
+        st.error("Veuillez entrer un Groupe valide en commençant par 'G'.")
         return
 
     data_dict, data_dict1 = load_data(classe)
-
     data = colo(groupe, semaine, data_dict, data_dict1)
 
     df = pd.DataFrame(data, columns=["Professeur", "Jour", "Heure", "Salle", "Matière"])
-    df.index = ['' for i in range(len(df))]
-
+    df.index = ['' for _ in range(len(df))]
     st.table(df.style.hide(axis='index'))
 
 
 def main():
-    if st.sidebar.button('EDT EPS'):
-        st.image("EPS_page-0001.jpg", caption="EDT EPS TSI1")
-        st.image("EPS_page-0002.jpg", caption="EDT EPS TSI2")
-
     st.sidebar.header("Sélection")
 
-    classe = st.sidebar.selectbox("TSI", options=["1", "2"], index=0)
-    groupe = st.sidebar.text_input("Groupe", value=load_settings()[0])
-    semaine = st.sidebar.text_input("Semaine", value=load_settings()[1])
+    # Définir la date de début de la première semaine
+    start_date = datetime.strptime("16/09/2024", "%d/%m/%Y")
+    current_date = datetime.now()
+    first_day_of_week = get_week_start_date(start_date, current_date)
+    end_date = first_day_of_week + timedelta(days=6)
 
-    # Configuration de la date de début et de la date actuelle
-    start_date = datetime.strptime("16/09/2024", "%d/%m/%Y")  # Début de la première semaine
-    current_date = datetime.now()  # Date actuelle
-
-    # Calcul dynamique de la semaine courante ou suivante
-    while current_date > start_date + timedelta(days=6):  # Si hors intervalle de la semaine actuelle
-        start_date += timedelta(days=7)  # Passer à la semaine suivante
-
-    end_date = start_date + timedelta(days=6)  # Fin de la semaine courante
-
-    st.sidebar.write(f"Premier jour de la semaine : {start_date.strftime('%d/%m/%Y')}")
+    st.sidebar.write(f"Premier jour de la semaine : {first_day_of_week.strftime('%d/%m/%Y')}")
     st.sidebar.write(f"Dernier jour de la semaine : {end_date.strftime('%d/%m/%Y')}")
 
+    classe = st.sidebar.selectbox("TSI", options=["1", "2"], index=0)
+    groupe = st.sidebar.text_input("Groupe", value="G1")
+    semaine = st.sidebar.text_input("Semaine", value="1")
 
-    if start_date <= current_date <= end_date:
-        first_day_of_week = get_start_of_week(current_date)
-        st.sidebar.write(f"Premier jour de la semaine : {first_day_of_week}")
-    else:
-        st.sidebar.write("Date hors intervalle.")
+    if st.sidebar.button("Afficher"):
+        st.session_state.groupe = groupe
+        st.session_state.semaine = semaine
+        st.session_state.classe = classe
+        display_data()
 
-    if st.sidebar.button("Afficher", on_click=display_data):
-        st.info("""Veuillez verifier quand même de temps en temps votre colloscope papier, pour verifier si il n'y a pas d'erreur""", icon="⚠️")
     st.markdown(
         """
-        <div style="position: fixed ; center: 0; width: 100%; font-size: 10px;">
+        <div style="position: fixed; bottom: 0; width: 100%; font-size: 10px;">
             Fait par BERRY Mael, avec l'aide de SOUVELAIN Gauthier et de DAMBRY Paul
         </div>
         """,
-        unsafe_allow_html=True
+        unsafe_allow_html=True,
     )
-
-    st.session_state.groupe = groupe
-    st.session_state.semaine = semaine
-    st.session_state.classe = classe
 
 
 if __name__ == "__main__":
