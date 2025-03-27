e28f6ba9383431751e17acf98a089a5d92a32e7f
Wamei154 <57544490+Wamei154@users.noreply.github.com>
2025-03-27 19:54:51 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import pandas as pd
 from datetime import datetime
 
 st.logo("logo_prepa.png", size='large')
-st.sidebar.link_button("Site du lycée", "https://sites.google.com/site/cpgetsimarcelsembat/")
+st.sidebar.link_button("Site du lycée", "https://sites.google.com/site/cpgetsimarcelsembat/", icon='logo_prepa.png')
 
 def chemin_ressource(chemin_relatif):
     """Retourne le chemin vers la ressource"""
