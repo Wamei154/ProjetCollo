574b807ec6b909c2860a082fb5cceacabebb96cf
Wamei154 <57544490+Wamei154@users.noreply.github.com>
2025-03-27 22:26:58 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ except FileNotFoundError:
     st.stop()
 
 html_code = f'''
-<div style="text-align: center; margin-bottom: 50px;">
+<div style="text-align: center; margin-bottom: 100px;">
     <a href="https://sites.google.com/site/cpgetsimarcelsembat/" target="_blank">
         <img src="data:image/png;base64,{b64_data}" width="150">
     </a>
